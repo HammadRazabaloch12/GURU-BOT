417c585c2ae3527747edba27f04143fd5c2dfb56
HammadRazabaloch12 <163525395+HammadRazabaloch12@users.noreply.github.com>
2024-04-01 13:20:22 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['923470648814', 'Baloch online services provider', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
